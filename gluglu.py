795b6898841e331e127889dc38212611578acc3d
Laplace-py <henricgil@discente.com>
2022-02-14 18:33:45 -0300

    actually working version

--- gluglu.py
+++ gluglu.py
@@ -4,11 +4,14 @@
 import pandas as pd
 from chembl_webresource_client.new_client import new_client
 #import matplotlib.pyplot as plt
-import numpy as np
-def main():
-  fulllist = []
+#import numpy as np
 
-  def get_chembl_targs(endpoint,act="IC50"):
+def main():
+  keys = ['molecule_chembl_id', 'canonical_smiles', 
+          'standard_type', 'standard_units', 'standard_relation', 
+          'standard_value', 'assay_description']
+  
+  def get_chembl_targs(endpoint="hERG",act="IC50"):
     """inhibition,ki,ec50"""
     activity = new_client.activity
     
@@ -17,19 +20,21 @@ def main():
     herg_act = activity.filter(target_chembl_id=herg['target_chembl_id']).filter(standard_type=act)
     return herg_act
 
-  keys = ['molecule_chembl_id', 'canonical_smiles', 
-          'standard_type', 'standard_units', 'standard_relation', 
-          'standard_value', 'assay_description']
-
   def dct_sorter(dct_list,keys_to_look_for=keys): 
       """Maps entry function"""
       a={}
-      
+      #print(dct_list)
       for key in keys_to_look_for:
         a[key]=dct_list[key]
+      return a
 
-      fulllist.append(a)
+  full=list(map(dct_sorter,get_chembl_targs("hERG","IC50")))
   
-  map(dct_sorter,get_chembl_targs("hERG","IC50"))
+  print(full)
   
-  pd.DataFrame(fulllist)
+  df=pd.DataFrame(full)
+  
+  df.to_csv("test.csv")
+
+if __name__ == '__main__':
+  main()
