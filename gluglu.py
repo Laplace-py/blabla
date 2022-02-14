58b68bf8ee68a49f6d8c5dd025b6c355275eb364
Laplace-py <henricgil@discente.com>
2022-02-14 17:25:28 -0300

    Com a map talvez seja mais rapido

--- gluglu.py
+++ gluglu.py
@@ -1,27 +1,35 @@
-!pip install chembl_webresource_client
-!pip install 
+#!pip install chembl_webresource_client
+#!pip install 
 
 import pandas as pd
 from chembl_webresource_client.new_client import new_client
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
+def main():
+  fulllist = []
 
-target = new_client.target
-activity = new_client.activity
-herg = target.filter(pref_name__iexact='hERG').only('target_chembl_id')[0]
-herg_ic50 = activity.filter(target_chembl_id=herg['target_chembl_id']).filter(standard_type="IC50")
-herg_inhibition = activity.filter(target_chembl_id=herg['target_chembl_id']).filter(standard_type="Inhibition")
-herg_ki = activity.filter(target_chembl_id=herg['target_chembl_id']).filter(standard_type="Ki")
-herg_ec50 = activity.filter(target_chembl_id=herg['target_chembl_id']).filter(standard_type="EC50")
+  def get_chembl_targs(endpoint,act="IC50"):
+    """inhibition,ki,ec50"""
+    activity = new_client.activity
+    
+    herg = new_client.target.filter(pref_name__iexact=endpoint).only('target_chembl_id')[0]
+    
+    herg_act = activity.filter(target_chembl_id=herg['target_chembl_id']).filter(standard_type=act)
+    return herg_act
 
-keys = ['molecule_chembl_id', 'canonical_smiles', 
-        'standard_type', 'standard_units', 'standard_relation', 
-        'standard_value', 'assay_description']
-        
+  keys = ['molecule_chembl_id', 'canonical_smiles', 
+          'standard_type', 'standard_units', 'standard_relation', 
+          'standard_value', 'assay_description']
 
-fulllist = []
-for item in range (len(herg_ic50)+1):
-  a = [herg_ic50[item].get(key) for key in keys]
-  fulllist.append(a)
+  def dct_sorter(dct_list,keys_to_look_for=keys): 
+      """Maps entry function"""
+      a={}
+      
+      for key in keys_to_look_for:
+        a[key]=dct_list[key]
+
+      fulllist.append(a)
   
-pd.DataFrame(fulllist)
+  map(dct_sorter,get_chembl_targs("hERG","IC50"))
+  
+  pd.DataFrame(fulllist)
